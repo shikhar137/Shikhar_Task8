c4c53a90ebad1ccd618f671142575d8c86d5bbd6
shikhar137 <shikhar171737.cse@chitkara.edu.in>
2020-06-17 16:01:03 +0530

    first commit

---
+++ src/test/java/RemoveFirst2AChars/AppTest.java
@@ -0,0 +1,20 @@
+package RemoveFirst2AChars;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
